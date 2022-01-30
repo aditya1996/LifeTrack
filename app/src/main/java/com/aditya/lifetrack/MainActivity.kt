101e920339908263aaba5d40afa7d79d9fe619db
aditya1996 <vineettiwari96@gmail.com>
2022-01-30 21:22:14 +0530

    FloatingActionButton

--- app/src/main/java/com/aditya/lifetrack/MainActivity.kt
+++ app/src/main/java/com/aditya/lifetrack/MainActivity.kt
@@ -9,15 +9,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private var recyclerView:RecyclerView? = null
     private var gridLayoutManager:GridLayoutManager? = null
     private var recyclerViewAdapter : RecyclerViewAdapter? = null
-    private lateinit var btnEnter : Button
-    private lateinit var etActivity: EditText
-    private lateinit var btnDelete: Button
+    private lateinit var btnFab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,9 +25,7 @@ class MainActivity : AppCompatActivity() {
         val db = DatabaseHelper(this)
         val data = db.readData()
 
-        btnEnter = findViewById(R.id.btnEnter)
-        etActivity = findViewById(R.id.etActivity)
-        btnDelete = findViewById(R.id.btnDelete)
+        btnFab = findViewById(R.id.btnFab)
         recyclerView = findViewById(R.id.recyclerView)
         gridLayoutManager = GridLayoutManager(applicationContext, 2, LinearLayoutManager.VERTICAL, false)
         recyclerView?.layoutManager = gridLayoutManager
@@ -36,26 +33,31 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView?.adapter = recyclerViewAdapter
 
-        btnEnter.setOnClickListener{
-            if (etActivity.text.toString().isNotEmpty()) {
-                val id = 0
-                val activity = Activity(etActivity.text.toString(), 0)
-                db.insertData(activity)
-                etActivity.text.clear()
-            } else {
-                Toast.makeText(this, "Please Fill All Data's", Toast.LENGTH_SHORT).show()
-            }
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+        btnFab.setOnClickListener{
+            var dialog = CustomDialogActivity()
+            dialog.show(supportFragmentManager, "customDialog")
         }
 
-        btnDelete.setOnClickListener{
-            db.deleteData()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
+//        btnEnter.setOnClickListener{
+//            if (etActivity.text.toString().isNotEmpty()) {
+//                val id = 0
+//                val activity = Activity(etActivity.text.toString(), 0)
+//                db.insertData(activity)
+//                etActivity.text.clear()
+//            } else {
+//                Toast.makeText(this, "Please Fill All Data's", Toast.LENGTH_SHORT).show()
+//            }
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+//
+//        btnDelete.setOnClickListener{
+//            db.deleteData()
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
 
     }
 }
